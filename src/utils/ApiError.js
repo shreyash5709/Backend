23b70a2866ff124b79c6c9f18bf93ee31b7391d1
Shreyash Jaiswal <shreyash5709@gmail.com>
2026-02-20 18:06:49 +0530

    missing word fix

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -1,12 +1,12 @@
 class ApiError extends Error {
     constructor(
-        stausCode,
+        statusCode,
         message="Something went wrong",
         errors=[],
         stack=""
     ){
         super(message)
-        this.stausCode = stausCode
+        this.statusCode = statusCode
         this.message = message
         this.errors = errors
         this.data = null
