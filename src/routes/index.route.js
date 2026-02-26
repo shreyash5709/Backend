2953cf9aa21caf67414cc5efdeedddb89321cf84
Shreyash Jaiswal <shreyash5709@gmail.com>
2026-02-26 21:29:59 +0530

    feat: create index file for route exports

---
+++ src/routes/index.route.js
@@ -0,0 +1,9 @@
+import userRouter from "./user.routes.js"
+import videoRouter from "./video.routes.js"
+import commentRouter from "./comment.routes.js"
+import likeRouter from "./like.routes.js"
+import playlistRouter from "./playlist.routes.js"
+import tweetRouter from "./tweet.routes.js"
+import subscriptionRouter from "./subscription.routes.js"
+
+export {userRouter, videoRouter, commentRouter, likeRouter, playlistRouter, tweetRouter, subscriptionRouter}
