245f688c83104d858b488299ed03076879d8fb41
Shreyash Jaiswal <shreyash5709@gmail.com>
2026-02-26 21:29:43 +0530

    feat: add index file for model exports

---
+++ src/models/index.model.js
@@ -0,0 +1,9 @@
+import { Comment } from "./comment.model.js";
+import { Like } from "./like.model.js";
+import { Playlist } from "./playlist.model.js";
+import { Subscription } from "./subscription.model.js";
+import { Tweet } from "./tweet.model.js";
+import { User } from "./user.model.js";
+import { Video } from "./video.model.js";  
+
+export {Comment, Like, Playlist, Subscription, Tweet, User, Video}
