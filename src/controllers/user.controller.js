123172d9fb4bae669124300941ef20d54fba576c
Shreyash Jaiswal <shreyash5709@gmail.com>
2026-02-26 12:38:32 +0530

    add route imports for likes, subscriptions, playlists, and tweets; import mongoose in user controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,8 @@ import {User} from "../models/user.model.js"
 import {uploadOnCloudinary, deleteFromCloudinary} from "../utils/cloudinary.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
 import jwt from "jsonwebtoken"
+import mongoose from "mongoose"
+
 
 
 const generateAccessAndRefreshToken = async(userId) => {
