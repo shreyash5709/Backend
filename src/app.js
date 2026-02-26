03f4dddd75e60d9d61d7ca8813fa58d4a4cfc8a0
Shreyash Jaiswal <shreyash5709@gmail.com>
2026-02-26 18:21:38 +0530

    refactor: remove unused route imports for comments, likes, subscriptions, playlists, and tweets

--- src/app.js
+++ src/app.js
@@ -25,19 +25,9 @@ app.use(cookieParser())
 //  routes imports
 import userRouter from "./routes/user.routes.js"
 import videoRouter from "./routes/video.routes.js"
-import commentRouter from "./routes/comment.routes.js"
-import likeRouter from "./routes/like.routes.js"
-import subscriptionRouter from "./routes/subscription.routes.js"
-import playlistRouter from "./routes/playlist.routes.js"
-import tweetRouter from "./routes/tweet.routes.js"
 
 // route declaration
 app.use("/api/v1/users", userRouter)
 app.use("/api/v1/videos", videoRouter)
-app.use("/api/v1/comments", commentRouter)
-app.use("/api/v1/likes", likeRouter)
-app.use("/api/v1/subscriptions", subscriptionRouter)
-app.use("/api/v1/playlists", playlistRouter)
-app.use("/api/v1/tweets", tweetRouter)
 
 export { app };
