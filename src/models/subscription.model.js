284515d0134566af0a114a1858d5588cd323c545
Shreyash Jaiswal <shreyash5709@gmail.com>
2026-02-27 16:46:19 +0530

    feat: add mongoose aggregate paginate plugin to subscription model

--- src/models/subscription.model.js
+++ src/models/subscription.model.js
@@ -1,4 +1,6 @@
 import {Schema, model} from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
 
 const subscriptionSchema = new Schema({
     subscriber: {
@@ -10,5 +12,7 @@ const subscriptionSchema = new Schema({
         ref: "User",
     }
 }, {timestamps: true})
+
+subscriptionSchema.plugin(mongooseAggregatePaginate);
 
 export const Subscription = model("Subscription", subscriptionSchema);
