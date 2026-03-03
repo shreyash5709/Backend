c46473a7f45ca08c23258173f2e7245817e015ad
Shreyash Jaiswal <shreyash5709@gmail.com>
2026-03-03 18:05:48 +0530

    playlist and comment route added

--- src/app.js
+++ src/app.js
@@ -34,5 +34,6 @@ app.use("/api/v1/videos", videoRouter)
 app.use("/api/v1/subscription", subscriptionRouter)
 app.use("/api/v1/likes", likeRouter)
 app.use("/api/v1/tweets", tweetRouter)
-
+app.use("/api/v1/playlists", playlistRouter)
+app.use("/api/v1/comments", commentRouter)
 export { app };
